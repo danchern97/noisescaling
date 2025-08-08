f18419232505c7520043bf6a2f277dc06e8e9246
Michał Mazuryk <mmazuryk1@int6.local.snellius.surf.nl>
2025-08-08 15:16:37 +0200

    sudokucnn changes

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
-#SBATCH --job-name=Train_Sudoku
+#SBATCH --job-name=Train_Sudoku_16
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=01:00:00
-#SBATCH --output=outfiles/sudokucnn_mean_2_2_orth02_%A.out
+#SBATCH --time=05:00:00
+#SBATCH --output=outfiles/sudokucnn_mean_16_2_orth01_%A.out
 
 
 module purge
@@ -14,4 +14,4 @@ module load 2023
 module load Anaconda3/2023.07-2
 #module load CUDA/12.4.0
 
-python -m train --config configs/orth_loss/scaler_mean_2_2_orth02.yaml
+python -m train --config configs/n_trans/scaler_mean_16_2_orth01.yaml
