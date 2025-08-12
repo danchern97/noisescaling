6721640e2e4c46fd61144e165e0640a7f7e87db1
Michal Mazuryk <michalm326@gmail.com>
2025-08-12 15:42:35 +0200

    reduce dist shift

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
-#SBATCH --job-name=Train_Sudoku_cross_2_3
+#SBATCH --job-name=Train_Sudoku_Mean
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=01:30:00
-#SBATCH --output=outfiles/sudokucnn_residual_attention_2_3_%A.out
+#SBATCH --time=02:30:00
+#SBATCH --output=outfiles/sudokucnn_dropout_mean_%A.out
 
 
 module purge
@@ -14,4 +14,8 @@ module load 2023
 module load Anaconda3/2023.07-2
 #module load CUDA/12.4.0
 
-python -m train --config configs/from_baseline/attention/scaler_2_3.yaml
+python -m train --config configs/from_baseline/mean/scaler_2_0.yaml
+python -m train --config configs/from_baseline/mean/scaler_2_1.yaml
+python -m train --config configs/from_baseline/mean/scaler_2_2.yaml
+python -m train --config configs/from_baseline/mean/scaler_2_3.yaml
+python -m train --config configs/from_baseline/mean/scaler_2_4.yaml
