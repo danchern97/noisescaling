8ca8c8bdcd4fd5d9303b969412803e8f5a0d73a9
Michal Mazuryk <michalm326@gmail.com>
2025-09-02 18:23:15 +0200

    new config

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
-#SBATCH --job-name=Train_Sudoku_Mean
+#SBATCH --job-name=Train_Sudoku_Attention
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=02:30:00
-#SBATCH --output=outfiles/sudokucnn_dropout_mean_%A.out
+#SBATCH --time=00:10:00
+#SBATCH --output=outfiles/sudokucnn_att_deepbaseline_%A.out
 
 
 module purge
@@ -14,8 +14,4 @@ module load 2023
 module load Anaconda3/2023.07-2
 #module load CUDA/12.4.0
 
-python -m train --config configs/from_baseline/mean/scaler_2_0.yaml
-python -m train --config configs/from_baseline/mean/scaler_2_1.yaml
-python -m train --config configs/from_baseline/mean/scaler_2_2.yaml
-python -m train --config configs/from_baseline/mean/scaler_2_3.yaml
-python -m train --config configs/from_baseline/mean/scaler_2_4.yaml
+python -m train --config configs/from_baseline/attention/scaler_2_0.yaml
